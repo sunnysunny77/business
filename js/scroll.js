5b9eb80dbf9000567e8e7607e5b88de717072e22
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-06-28 03:05:14 +0800

    up

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,8 @@ function handleScroll() {
     const figureOne = document.getElementById("figureOne");
     const figureTwo = document.getElementById("figureTwo");
     const section = document.getElementById("section3");
-    const line = section.offsetTop + section.offsetHeight * 25 / 100 - figureOne.offsetHeight / 2 - figureTwo.offsetHeight / 2;
+    //top quater sticky height 25vh, + 25vh / 100vh = 50vh, minus half the figure height to find the middle
+    const line = section.offsetTop + section.offsetHeight * 0.25 - figureOne.offsetHeight / 2 - figureTwo.offsetHeight / 2;
     var scrollY = window.scrollY;
     if (scrollY < line) {
         figureOne.className = "block";
