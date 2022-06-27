b6f46cb324c105f10a3f6ee460bad0a60016328b
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-06-28 00:10:40 +0800

    up

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+function handleScroll() {
+    const figureOne = document.getElementById("figureOne");
+    const figureTwo = document.getElementById("figureTwo");
+    const section = document.getElementById("section3");
+    const line = section.offsetTop + section.offsetHeight * 25 / 100 - figureOne.offsetHeight / 2 - figureTwo.offsetHeight / 2;
+    var scrollY = window.scrollY;
+    if (scrollY < line) {
+        figureOne.className = "block";
+        figureTwo.className = "none";
+    }
+    if (scrollY > line) {
+        figureOne.className = "none";
+        figureTwo.className = "block";
+    }
+}
+
+function eventListner(obj, type, callback, opt) {
+    if (obj) {
+        obj.addEventListener(type, callback, opt);
+    }
+}
+
+eventListner(window, "scroll", handleScroll, { passive: true });
