38e957dd742e59a5f25a4f86c5f239a8764071b0
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-12-20 18:19:58 +0800

    up

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,30 @@
 const nav = document.getElementById("nav");
 const button = document.getElementsByClassName("button");
+const main = document.querySelector("main");
+main.style.marginTop = nav.offsetHeight + "px";
+
+function handleNav() {
+    let scrollY = window.scrollY;
+
+    if (scrollY >= main.offsetTop && window.innerWidth > 576) {
+
+        nav.classList.add("sticky");
+        nav.classList.remove("absolute");
+        main.style.marginTop = 0;
+    } else if (window.innerWidth > 576)  {
+        nav.classList.add("absolute");
+        nav.classList.remove("sticky");
+        main.style.marginTop = nav.offsetHeight + "px";
+    } else if (window.innerWidth <= 576) {
+        nav.classList.remove("sticky");
+    }
+
+
+}
 
 function handleTouch() {
 
-    [...button].forEach(function (button) {
+    [...button].forEach(function (button) {images/section1.jpg
         button.classList.add("display");
     });
 }
@@ -18,3 +39,5 @@ function handleMove() {
 nav.addEventListener("touchstart", handleTouch);
 
 document.addEventListener("touchmove", handleMove);
+
+window.addEventListener("scroll", handleNav);
