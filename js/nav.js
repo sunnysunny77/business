7b6250a9f50224e258454f624ced8f9f471387c3
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-12-20 18:22:25 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -12,14 +12,14 @@ function handleNav() {
         nav.classList.remove("absolute");
         main.style.marginTop = 0;
     } else if (window.innerWidth > 576)  {
+
         nav.classList.add("absolute");
         nav.classList.remove("sticky");
         main.style.marginTop = nav.offsetHeight + "px";
     } else if (window.innerWidth <= 576) {
+
         nav.classList.remove("sticky");
     }
-
-
 }
 
 function handleTouch() {
