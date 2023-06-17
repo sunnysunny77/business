5bb7555a026ee0aed8e1349d748be32607685b4a
daniel <contractor@lateralaspect.com.au>
2023-06-17 18:14:43 +0800

    up

--- js/nav.js
+++ js/nav.js
@@ -1,24 +1,24 @@
-const nav = document.getElementById("nav");
+const nav = document.querySelector("#nav");
 const main = document.querySelector("main");
 
 function handleNav() {
-    let scrollY = window.scrollY;
+  let scrollY = window.scrollY;
 
-    if (scrollY >= main.offsetTop - nav.offsetHeight && window.innerWidth > 576) {
+  if (scrollY >= main.offsetTop - nav.offsetHeight && window.innerWidth > 576) {
 
-        nav.classList.add("sticky");
-        nav.classList.remove("absolute");
-        main.style.marginTop = 0;
-    } else if (window.innerWidth > 576)  {
+    nav.classList.add("sticky");
+    nav.classList.remove("absolute");
+    main.style.marginTop = 0;
+  } else if (window.innerWidth > 576)  {
 
-        nav.classList.add("absolute");
-        nav.classList.remove("sticky");
-        main.style.marginTop = nav.offsetHeight + "px";
-    } else if (window.innerWidth <= 576) {
-
-        nav.classList.remove("absolute");
-        nav.classList.remove("sticky");
-    }
+    nav.classList.add("absolute");
+    nav.classList.remove("sticky");
+    main.style.marginTop = `${nav.offsetHeight  }px`;
+  } else if (window.innerWidth <= 576) {
+    main.style.marginTop = 0;
+    nav.classList.remove("absolute");
+    nav.classList.remove("sticky");
+  }
 }
 
 window.addEventListener("scroll", handleNav);
