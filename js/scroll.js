96ef10612759fbd2621d6bdb2ac90546dfb1565e
daniel <contractor@lateralaspect.com.au>
2023-06-20 12:18:35 +0800

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,18 @@
 function handleScroll() {
-    const figureOne = document.getElementById("figureOne");
-    const figureTwo = document.getElementById("figureTwo");
-    const section = document.getElementById("section3");
-    //top quater sticky height 25vh, + 25vh / 100vh = 50vh, minus half the figure height to find the middle
-    const line = section.offsetTop + section.offsetHeight * 0.25 - figureOne.offsetHeight / 2 - figureTwo.offsetHeight / 2;
-    let scrollY = window.scrollY;
-    if (scrollY < line) {
-        figureOne.className = "block";
-        figureTwo.className = "none";
-    }
-    if (scrollY > line) {
-        figureOne.className = "none";
-        figureTwo.className = "block";
-    }
+  const figureOne = document.querySelector("#figureOne");
+  const figureTwo = document.querySelector("#figureTwo");
+  const section = document.querySelector("#section2");
+  //top quater sticky height 25vh, + 25vh / 100vh = 50vh, minus half the figure height to find the middle
+  const line = section.offsetTop + (section.offsetHeight * 0.25) - (figureOne.offsetHeight / 2) - (figureTwo.offsetHeight / 2);
+  let scrollY = window.scrollY;
+  if (scrollY < line) {
+    figureOne.setAttribute("class", "block");
+    figureTwo.setAttribute("class", "none");
+  }
+  if (scrollY > line) {
+    figureOne.setAttribute("class", "none");
+    figureTwo.setAttribute("class", "block");
+  }
 }
 
 window.addEventListener("scroll", handleScroll, { passive: true });
-  
