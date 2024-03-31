84b05b491474cb6a93112dc6def52beb56bb840f
daniel <contractor@lateralaspect.com.au>
2024-03-31 20:42:45 +0800

    up

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -9,21 +9,62 @@ const getPosition = (element) => {
 
 const handleScrollAnimation = () => {
 
+  const outer = document.querySelector(".outer");
+
+  let scrollPosOuter = window.scrollY + (window.innerHeight /  2) - (outer.offsetHeight / 2);
+
+  if (scrollPosOuter > getPosition(outer)  && !outer.classList.contains("animation") && window.innerWidth > 1023) {
+
+    outer.classList.add("animation");
+  }
+
+  if (scrollPosOuter < getPosition(outer)  && outer.classList.contains("animation") && window.innerWidth > 1023) {
+
+    outer.classList.remove("animation");
+  }
+
+  let scrollPosOuterTwo = window.scrollY + (window.innerHeight /  3) - (outer.offsetHeight / 2);
+
+
+  if (scrollPosOuterTwo > getPosition(outer)  && outer.classList.contains("animation") && window.innerWidth > 1023) {
+
+    outer.classList.remove("animation");
+
+  }
+
   const image = document.querySelectorAll(".scroll");
 
   for (let i in image) {
 
-    let scrollPos = window.scrollY + window.innerHeight - image[i].offsetHeight - (image[i].offsetHeight / 3);
+    let scrollPos = window.scrollY + window.innerHeight  - image[i].offsetHeight - (image[i].offsetHeight / 2);
 
-    if (scrollPos > getPosition(image[i]) && !image[i].classList.contains("animation")) {
+    if (scrollPos > getPosition(image[i]) && !image[i].classList.contains("animation") && window.innerWidth < 1023) {
 
       image[i].classList.add("animation");
     }
-    if (scrollPos < getPosition(image[i]) && image[i].classList.contains("animation")) {
+    if (scrollPos < getPosition(image[i]) && image[i].classList.contains("animation") && window.innerWidth < 1023) {
 
       image[i].classList.remove("animation");
     }
+
   }
+
+  const imageTwo = document.querySelectorAll(".scrollTwo");
+
+  for (let i in imageTwo) {
+
+    let scrollPos = window.scrollY + window.innerHeight - imageTwo[i].offsetHeight - (imageTwo[i].offsetHeight / 3);
+
+    if (scrollPos > getPosition(imageTwo[i]) && !imageTwo[i].classList.contains("animation")) {
+
+      imageTwo[i].classList.add("animation");
+    }
+    if (scrollPos < getPosition(imageTwo[i]) && imageTwo[i].classList.contains("animation")) {
+
+      imageTwo[i].classList.remove("animation");
+    }
+  }
+
 };
 
 window.addEventListener("scroll", handleScrollAnimation);
