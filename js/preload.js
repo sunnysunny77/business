bac301c5bcda3825c87256e81fad90799550b3f1
daniel <contractor@lateralaspect.com.au>
2024-06-30 17:18:59 +0800

    up

---
+++ js/preload.js
@@ -0,0 +1,11 @@
+let head = document.querySelector("head");
+let link = document.createElement("link");
+link.rel = "preload";
+link.as = "image";
+if (window.screen.width > 576) {
+  link.href = "./images/header.webp";
+  head.append(link);
+} else {
+  link.href = "./images/headerm.webp";
+  head.append(link);
+}
